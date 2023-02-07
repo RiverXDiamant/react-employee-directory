57fcc77fc2ae3318d00284bfd4fd007c5904c981
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-07 09:10:53 -0500

    added logo to navigation bar

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -5,9 +5,22 @@ export default function Nav() {
   return (
     <>
       <div className="nav">
-        <Link to="/">Home</Link>
-        &nbsp;&nbsp;
-        <Link to="/directory">Directory</Link>
+        <div className="nav-logo">
+          <img
+            src={process.env.PUBLIC_URL + "images/auburn-medical-logo.png"}
+            alt="auburn medical logo"
+            width={100}
+            height={100}
+          />
+        </div>
+
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          &nbsp;&nbsp;
+          <Link to="/directory">Directory</Link>
+          &nbsp;&nbsp;
+          <Link to="#contactus">Contact Us</Link>
+        </div>
       </div>
     </>
   );
