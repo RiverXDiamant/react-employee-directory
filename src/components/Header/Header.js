c317c481ed994e400d2b4497a7dc125ca6156375
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-09 09:20:15 -0500

    renamed HomePageHeader.js to Header.js

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+// components
+import React from "react";
+
+// Chakra components
+import { Box, Text } from "@chakra-ui/react";
+
+export default function Header() {
+  return (
+    <Box>
+      <Text> Header Component</Text>
+    </Box>
+  );
+}
--- src/components/HomePage/HomePageHeader.js
+++ src/components/HomePage/HomePageHeader.js
@@ -2,7 +2,7 @@ export default function HomePageHeader() {
   return (
     <>
       <div className="homeHeader">
-        <h1> Home Page Component</h1>
+        <h1> Home Page Header Component</h1>
       </div>
     </>
   );
