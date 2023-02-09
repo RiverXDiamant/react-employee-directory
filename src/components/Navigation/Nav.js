79d327af613357b4071b73c552a6850d86f09120
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-09 14:56:21 -0500

    add AMC Logo to Nav and added styling

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,34 +1,37 @@
 // components
 import React from "react";
 import { Link } from "react-router-dom";
+import AMCLogo from "../../img/auburn-medical-logo.png";
 
 // Chakra components
-import { Box, Flex, HStack } from "@chakra-ui/react";
+import { Box, Flex, HStack, Image } from "@chakra-ui/react";
 
 export default function Nav() {
   return (
     <>
-      <Box>
-        <Flex justify="center">
-          <HStack>
-            <Box>
-              <img
-                src={process.env.PUBLIC_URL + "images/auburn-medical-logo.png"}
-                alt="auburn medical logo"
-                width={100}
-                height={100}
-              />
-            </Box>
+      <Box h="150">
+        <Box as="nav">
+          <Flex justify="center">
+            <HStack>
+              <Box>
+                <Image
+                  src={AMCLogo}
+                  alt="Auburn Medical Center logo"
+                  h="150"
+                  w="200"
+                />
+              </Box>
 
-            <Box>
-              <Link to="/">Home</Link>
-              &nbsp;&nbsp;
-              <Link to="/directory">Directory</Link>
-              &nbsp;&nbsp;
-              <Link to="#contactus">Contact Us</Link>
-            </Box>
-          </HStack>
-        </Flex>
+              <Box>
+                <Link to="/">Home</Link>
+                &nbsp;&nbsp;
+                <Link to="/directory">Directory</Link>
+                &nbsp;&nbsp;
+                <Link to="#contactus">Contact Us</Link>
+              </Box>
+            </HStack>
+          </Flex>
+        </Box>
       </Box>
     </>
   );
