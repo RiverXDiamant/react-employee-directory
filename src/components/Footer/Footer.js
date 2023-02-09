a9a810a553917296b517177066643f4251795867
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-09 11:16:38 -0500

    created Footer dir and component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+// components
+import React from "react";
+
+// Chakra components
+import { Box, Flex, Text } from "@chakra-ui/react";
+
+export default function Footer() {
+  return (
+    <Box>
+      <Flex justify="center">
+        <Text fontSize="xl">Footer Component</Text>
+      </Flex>
+    </Box>
+  );
+}
