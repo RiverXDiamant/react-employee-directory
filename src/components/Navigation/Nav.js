7e89a18bcad9166970f30cfa2c6881d259726f0d
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-09 10:52:47 -0500

    imported Chakra components

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,27 +1,35 @@
-// link component
+// components
+import React from "react";
 import { Link } from "react-router-dom";
+
+// Chakra components
+import { Box, Flex, HStack } from "@chakra-ui/react";
 
 export default function Nav() {
   return (
     <>
-      <div className="nav">
-        <div className="nav-logo">
-          <img
-            src={process.env.PUBLIC_URL + "images/auburn-medical-logo.png"}
-            alt="auburn medical logo"
-            width={100}
-            height={100}
-          />
-        </div>
+      <Box>
+        <Flex justify="center">
+          <HStack>
+            <Box>
+              <img
+                src={process.env.PUBLIC_URL + "images/auburn-medical-logo.png"}
+                alt="auburn medical logo"
+                width={100}
+                height={100}
+              />
+            </Box>
 
-        <div className="nav-links">
-          <Link to="/">Home</Link>
-          &nbsp;&nbsp;
-          <Link to="/directory">Directory</Link>
-          &nbsp;&nbsp;
-          <Link to="#contactus">Contact Us</Link>
-        </div>
-      </div>
+            <Box>
+              <Link to="/">Home</Link>
+              &nbsp;&nbsp;
+              <Link to="/directory">Directory</Link>
+              &nbsp;&nbsp;
+              <Link to="#contactus">Contact Us</Link>
+            </Box>
+          </HStack>
+        </Flex>
+      </Box>
     </>
   );
 }
