ef0c813fc6e3f03b9b2af5a163cae1c0e27d8748
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-09 10:50:37 -0500

    refactored to use Chakra UI; added Header text and external link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,21 @@
 import React from "react";
 
 // Chakra components
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Flex, Text, Link } from "@chakra-ui/react";
 
 export default function Header() {
   return (
-    <Box>
-      <Text> Header Component</Text>
+    <Box bg="blue.700" color="white" _hover="gray.50">
+      <Flex justify="center">
+        <Text fontSize="2xl" fontWeight="bold">
+          <Link
+            href="https://www.cdc.gov/coronavirus/2019-nCoV/index.html"
+            isExternal
+          >
+            COVID-19 RESOURCES, SYMPTOM CHECKER & SAFETY CENTER
+          </Link>
+        </Text>
+      </Flex>
     </Box>
   );
 }
