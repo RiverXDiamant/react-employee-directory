8039d5542bb52ddb99d6c344fcfac70005a03c46
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-14 12:43:45 -0500

    removed unused Chakra components

--- src/components/Home/CareOptions.js
+++ src/components/Home/CareOptions.js
@@ -1,43 +1,71 @@
 // components
+import React from "react";
 import {
-  Box,
-  Text,
-  Flex,
-  Stack,
-  HStack,
-  Icon,
-  StackProps,
-  Button,
-} from "@chakra-ui/react";
+  FaBookMedical,
+  FaAddressBook,
+  FaMoneyCheckAlt,
+  FaHospital,
+} from "react-icons/fa";
+
+import { Link } from "react-router-dom";
+
+import { Box, Text, Flex, Stack, Button } from "@chakra-ui/react";
+
+// Button List Items
+
+export const ListItem = (props) => {
+  const { children, ...rest } = props;
+
+  return (
+    <Stack as="li" spacing="5px" {...rest} alignItems="center">
+      <Text textAlign="center">{children}</Text>
+    </Stack>
+  );
+};
 
 export default function CareOptions() {
   return (
     <Box bg="gray.300" h="300" w="4xl" mt="10" textAlign="center">
       <Flex justify="center">
-        <Box bg="blue.200" h="300" mr="5">
+        <Box bg="blue.200" h="100" mr="5">
+          <Link to="/directory">
+            <Button colorScheme="blue" size="lg">
+              <FaBookMedical /> &nbsp;&nbsp;Find A Doctor
+            </Button>
+          </Link>
+
+          <Text as="p">Choose a Provider</Text>
+        </Box>
+
+        <Box bg="blue.200" h="100" mr="5">
           <Button colorScheme="blue" size="lg">
-            Find A Doctor
+            <FaAddressBook />
+            &nbsp;&nbsp; MyChart
           </Button>
-          <Box as="p">dlskajfkldf</Box>
+          <Text as="p">Login or Sign Up</Text>
+        </Box>
+
+        <Box bg="blue.200" h="200" mr="5">
+          <Button colorScheme="blue" size="lg">
+            <FaMoneyCheckAlt />
+            &nbsp;&nbsp; Pay Your Bill
+          </Button>
+
+          <ListItem>
+            <Link to="/">Online Bill Pay</Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/">Financial Assistance</Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/">FAQ</Link>
+          </ListItem>
         </Box>
 
         <Box bg="blue.200" h="300" mr="5">
           <Button colorScheme="blue" size="lg">
-            MyChart
-          </Button>
-          <Text>dsfdsfdsl</Text>
-        </Box>
-
-        <Box bg="blue.200" h="300" mr="5">
-          <Button colorScheme="blue" size="lg">
-            Pay Your Bill
-          </Button>
-          <Text>Lorem</Text>
-        </Box>
-
-        <Box bg="blue.200" h="300" mr="5">
-          <Button colorScheme="blue" size="lg">
-            Locations
+            <FaHospital />
+            &nbsp;&nbsp; Location
           </Button>
           <Text>Lorem</Text>
         </Box>
