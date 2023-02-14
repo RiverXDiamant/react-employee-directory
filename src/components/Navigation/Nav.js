fa55a601ffc1bc179dadb747c9989b5af7642832
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-14 10:49:11 -0500

    added Appointment button

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -4,31 +4,44 @@ import { Link } from "react-router-dom";
 import AMCLogo from "../../img/auburn-medical-logo.png";
 
 // Chakra components
-import { Box, Flex, HStack, Image } from "@chakra-ui/react";
+import { Box, Flex, Text, HStack, Image, Button } from "@chakra-ui/react";
 
 export default function Nav() {
   return (
     <>
-      <Box h="150">
+      <Box>
         <Box as="nav">
           <Flex justify="center">
             <HStack>
-              <Box>
+              <Box mr="100">
                 <Image
                   src={AMCLogo}
                   alt="Auburn Medical Center logo"
-                  h="150"
-                  w="200"
+                  h="300"
+                  w="300"
                 />
               </Box>
-
-              <Box>
-                <Link to="/">Home</Link>
-                &nbsp;&nbsp;
-                <Link to="/directory">Directory</Link>
-                &nbsp;&nbsp;
-                <Link to="#contactus">Contact Us</Link>
-              </Box>
+              <Text>
+                <Link to="/">Care and Treatment</Link>
+              </Text>
+              &nbsp;&nbsp;
+              <Text>
+                <Link to="/directory">Provider Directory</Link>
+              </Text>
+              &nbsp;&nbsp;
+              <Link to="/">Patient and Visitor Info</Link>
+              &nbsp;&nbsp;
+              <Link to="/">About Us</Link>
+              <Link to="/">
+                <Button
+                  size="md"
+                  colorScheme="red"
+                  color="white"
+                  _hover="black"
+                >
+                  Make An Appointment
+                </Button>
+              </Link>
             </HStack>
           </Flex>
         </Box>
