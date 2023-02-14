d5bcc091006aa37ee4af2fb8b52875cec24f96ad
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-14 10:57:56 -0500

    imported CareOptions component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,34 +1,27 @@
 // components
 import React from "react";
 import AMCImage from "../img/auburn-medical-front.jpg";
+import CareOptions from "../components/Home/CareOptions";
 
 // Chakra components
-import { Box, Text, HStack, Button, Flex, Image } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
 
 export default function HomePage() {
   return (
     <Box bg="gray.200">
       <Flex justify="center">
-        <Box as="section" bg="gray.500">
-          <Box as="figure">
-            <Image
-              src={AMCImage}
-              alt="Auburn Medical Center building"
-              h="500"
-              w="4xl"
-            />
-            <Box>
-              <Text
-                textAlign="center"
-                fontSize="2xl"
-                fontWeight="bold"
-                color="white"
-              >
-                Questions About Your Patient Portal Account?
-              </Text>
-            </Box>
-          </Box>
+        <Box as="figure">
+          <Image
+            src={AMCImage}
+            alt="Auburn Medical Center building"
+            h="500"
+            w="4xl"
+          />
         </Box>
+      </Flex>
+
+      <Flex justify="center">
+        <CareOptions />
       </Flex>
     </Box>
   );
