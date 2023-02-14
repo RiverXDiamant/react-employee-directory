8214936ae2a42133c7dbc5ce530a16e92e620c9e
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-14 10:57:17 -0500

    updated placeholder styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 
 export default function Footer() {
   return (
-    <Box as="footer" bg="purple.200" h="200">
+    <Box as="footer" bg="purple.200" h="400">
       <Flex justify="center">
         <Text fontSize="xl">Footer Component</Text>
       </Flex>
