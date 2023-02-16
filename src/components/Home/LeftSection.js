3655cd6c744c9fa9f5456707385fa4b8ae6b5745
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-16 10:20:03 -0500

    aded information text; completed section

--- src/components/Home/LeftSection.js
+++ src/components/Home/LeftSection.js
@@ -1,13 +1,34 @@
 //components
-import { Heading, Box, Flex } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { Heading, Box, Flex, Text } from "@chakra-ui/react";
 
 export default function LeftSectionBio() {
   return (
-    <Box bg="blue.700" h="300" w="md" mr="5">
-      <Flex justify="center">
+    <Box bg="blue.700" h="300" w="sm" mr="5">
+      <Flex justify="center" mt="2">
         <Heading fontWeight="bold" fontSize="xl" color="white">
           Why AMC?
         </Heading>
+      </Flex>
+      <Flex justify="center">
+        <Box as="hr" bg="blue.200" h="0.5" w="10" mb="2" mt="3"></Box>
+      </Flex>
+
+      <Box h="200" mt="5" mb="-3">
+        <Text fontSize="xl" fontWeight="light" color="white" ml="2">
+          We are here to work with you every step of the way to keep our
+          communities healthy. At your service are our nationally recognized
+          physicians, lead-edge technology and world-class facilities.
+          Excellence, caring and integrity guid our everyday behaviors.
+        </Text>
+      </Box>
+
+      <Flex justify="end" mr="3">
+        <Box>
+          <Link to="/">
+            <Text color="white">LEARN MORE</Text>
+          </Link>
+        </Box>
       </Flex>
     </Box>
   );
--- src/components/Home/RightSection.js
+++ src/components/Home/RightSection.js
@@ -1,15 +1,32 @@
 //components
 
-import { Heading, Box, Flex, Text } from "@chakra-ui/react";
+import { Heading, Box, Flex, Text, Link } from "@chakra-ui/react";
 
 export default function RightSectionPrivacy() {
   return (
-    <Box bg="blue.700" h="300" w="md">
-      <Flex justify="center">
+    <Box bg="blue.700" h="300" w="sm">
+      <Flex justify="center" mt="2">
         <Heading fontWeight="bold" fontSize="xl" color="white">
           Privacy Notice
         </Heading>
-        <Text> </Text>
+      </Flex>
+      <Flex justify="center">
+        <Box as="hr" bg="blue.200" h="0.5" w="10" mb="2" mt="4"></Box>
+      </Flex>
+
+      <Box h="200" mt="5" mb="-3">
+        <Text fontSize="xl" fontWeight="light" color="white" ml="2">
+          Auburn Medical Center (AMC) is committed to protecting the security
+          and privacy of our patient's information.{" "}
+        </Text>
+      </Box>
+
+      <Flex justify="end" mr="3">
+        <Box>
+          <Link to="/">
+            <Text color="white">LEARN MORE</Text>
+          </Link>
+        </Box>
       </Flex>
     </Box>
   );
