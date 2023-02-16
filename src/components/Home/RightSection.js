7bc2da6572ac688c8b39ecd8fca227b311e9be7b
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-16 14:39:38 -0500

    updated privacy statement to include notice of scam emails

--- src/components/Home/RightSection.js
+++ src/components/Home/RightSection.js
@@ -4,32 +4,33 @@ import { Heading, Box, Flex, Text, Link } from "@chakra-ui/react";
 
 export default function RightSectionPrivacy() {
   return (
-    <Box bg="blue.700" h="300" w="sm">
+    <Box bg="blue.700" h="300" w="sm" overflow="hidden">
       <Flex justify="center" mt="2">
         <Heading fontWeight="bold" fontSize="xl" color="white">
-          Privacy Notice
+          Notice of E-Mail Scams
         </Heading>
       </Flex>
       <Flex justify="center">
         <Box as="hr" bg="blue.200" h="0.5" w="10" mb="2" mt="4"></Box>
       </Flex>
 
-      <Box h="200" mt="5" mb="-3">
-        <Text fontSize="xl" fontWeight="light" color="white" ml="2">
+      <Box h="200" mt="1" mb="-3">
+        <Text fontSize="md" fontWeight="light" color="white" ml="2">
           Auburn Medical Center (AMC) is committed to protecting the security
-          and privacy of our patient's information.{" "}
+          and privacy of our patient's information. Please be aware of any
+          suspicious emails or communications from people claiming to be
+          representatives of Auburn Medical Center.
         </Text>
-      </Box>
 
-      <Flex justify="end" mr="3">
-        <Box>
-          <Link to="/">
-            <Text color="white" fontWeight="bold">
-              LEARN MORE
-            </Text>
-          </Link>
+        <Box bg="red.500" border="none">
+          <Text fontSize="md" fontWeight="light" color="white" ml="2">
+            Do not disclose financial or personal details to anyone you do not
+            know or continue to engage in communications you believe may be
+            fraudulent. If you believe you may have been targeted in this scam,
+            contact your local law enforcement.
+          </Text>
         </Box>
-      </Flex>
+      </Box>
     </Box>
   );
 }
