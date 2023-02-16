7f9c03c80b44417bd310f7dee058f92971a9113e
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-16 14:39:00 -0500

    added sections with dummy links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,49 @@
 // components
 import React from "react";
+import { Link } from "react-router-dom";
 import FooterNav from "./FooterNav";
+import AMCFooterLogo from "../../img/auburn-medical-footer-logo.png";
 
-import { LoremIpsum } from "react-lorem-ipsum";
+// import { LoremIpsum } from "react-lorem-ipsum";
 
 // Chakra components
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Stack, Text, Image, Button } from "@chakra-ui/react";
 
 export default function Footer() {
   return (
-    <Box as="footer" bg="purple.300" h="400">
+    <Box as="footer" h="500">
       <Flex justify="center">
-        <Text fontSize="xl">Footer Component</Text>
-      </Flex>
+        <Box h="250" w="200" mr="5" borderRight="1px">
+          <Image
+            src={AMCFooterLogo}
+            alt="Auburn Medical Center Logo"
+            h="300"
+            w="200"
+            mt="-8"
+          />
 
-      <Flex>
-        <LoremIpsum p={3} />
+          <Flex justify="center" mt="-20">
+            <Link to="/">
+              <Button
+                border="1px"
+                colorScheme="transparent"
+                color="blue.600"
+                _hover={{ background: "blue.600", color: "white" }}
+              >
+                About Us
+              </Button>
+            </Link>
+          </Flex>
+        </Box>
+
+        <Box bg="gray.300" h="250" w="300" borderRight="1px">
+          <Stack id="info">
+            <Text as="h3">AMC Health</Text>
+            <Link to="/">
+              <Text _hover={{ color: "blue.600" }}>Place holder</Text>
+            </Link>
+          </Stack>
+        </Box>
       </Flex>
 
       <FooterNav />
