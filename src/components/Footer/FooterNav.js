229b44ed5cbae83df013a40a2ac24facf252ed47
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-16 12:16:12 -0500

    styled nav links

--- src/components/Footer/FooterNav.js
+++ src/components/Footer/FooterNav.js
@@ -4,19 +4,43 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 
 export default function FooterNav() {
   return (
-    <Box as="nav" bg="blue.100" mt="20">
+    <Box as="nav" h="100" mt="20">
       <Flex justify="center">
-        <Link to="/">Accessibility |</Link>
-        &nbsp;&nbsp;
-        <Link to="/">Non-Discrimination |</Link>
-        &nbsp;&nbsp;
-        <Link to="/">Compliance |</Link>
-        &nbsp;&nbsp;
-        <Link to="/">Employees |</Link>
-        &nbsp;&nbsp;
-        <Link to="/">Careers |</Link>
-        &nbsp;&nbsp;
-        <Link to="/">Board Portal</Link>
+        <Link to="/">
+          <Text _hover={{ color: "blue.600", fontWeight: "bold" }}>
+            Accessibility
+          </Text>
+        </Link>
+        &nbsp; | &nbsp;
+        <Link to="/">
+          <Text _hover={{ color: "blue.600", fontWeight: "bold" }}>
+            Non-Discrimination
+          </Text>
+        </Link>
+        &nbsp; | &nbsp;
+        <Link to="/">
+          <Text _hover={{ color: "blue.600", fontWeight: "bold" }}>
+            Compliance
+          </Text>
+        </Link>
+        &nbsp; | &nbsp;
+        <Link to="/">
+          <Text _hover={{ color: "blue.600", fontWeight: "bold" }}>
+            Employees
+          </Text>
+        </Link>
+        &nbsp; | &nbsp;
+        <Link to="/">
+          <Text _hover={{ color: "blue.600", fontWeight: "bold" }}>
+            Careers
+          </Text>
+        </Link>
+        &nbsp; | &nbsp;
+        <Link to="/">
+          <Text _hover={{ color: "blue.600", fontWeight: "bold" }}>
+            Board Portal
+          </Text>
+        </Link>
       </Flex>
 
       <Box mt="2">
