c0f022c4a35eeb79f8a17589f3ba1a3d81e92b22
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-20 15:40:19 -0500

    imported SearchBar component

--- src/pages/EmployeeDirectoryPage.js
+++ src/pages/EmployeeDirectoryPage.js
@@ -3,11 +3,15 @@ import { Link } from "react-router-dom";
 
 // Chakra components
 import { Box, Flex, Text, Button, Stack } from "@chakra-ui/react";
+import SearchBar from "../components/Search/SearchBar";
 // import EmployeePage from "../pages/EmployeePage";
 
 export default function EmployeeDirectoryPage() {
   return (
-    <Box as="section" bg="gray.100" h="500">
+    <Box as="section" bg="gray.100" h="700">
+      <Flex justify="center">
+        <SearchBar />
+      </Flex>
       <Flex justify="center">
         <Stack>
           <Text>Employee Directory Page Component</Text>
