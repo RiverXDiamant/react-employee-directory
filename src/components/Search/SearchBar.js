f04a1b4f29c5ae425517e115b8505d35bf8f04dc
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-20 15:39:30 -0500

    added search input; styled input area and button

--- src/components/Search/SearchBar.js
+++ src/components/Search/SearchBar.js
@@ -1,12 +1,37 @@
+// components
+import { Box, Flex, Input, Button, HStack } from "@chakra-ui/react";
+import { FaSearch } from "react-icons/fa";
+
 export default function SearchBar() {
   return (
-    <>
-      <div className="searchBar">
-        <form>
-          <input type="text" name="search-employee" placeholder="John Smith" />
-          <input type="submit" value="search" />
-        </form>
-      </div>
-    </>
+    <Box>
+      <Flex justify="center">
+        <HStack>
+          <Box as="form">
+            <Input
+              type="text"
+              color="blue.700"
+              name="search-employee"
+              variant="filled"
+              placeholder="Find a Provider"
+              _placeholder={{ opacity: 1, color: "inherit" }}
+              size="lg"
+            />
+          </Box>
+          <Box bg="blue.400">
+            <Button
+              bg="blue.700"
+              color="white"
+              _hover={{ bg: "blue.600", fontWeight: "bold" }}
+              type="submit"
+              value="submit"
+            >
+              Search &nbsp;
+              <FaSearch />
+            </Button>
+          </Box>
+        </HStack>
+      </Flex>
+    </Box>
   );
 }
