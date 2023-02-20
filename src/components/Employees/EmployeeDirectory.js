ae02e5b2f5d0d8e76e4adea2e6d8c7ad864e9038
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-20 11:59:58 -0500

    added Employee info

--- src/components/Employees/EmployeeDirectory.js
+++ src/components/Employees/EmployeeDirectory.js
@@ -1,9 +1,53 @@
-export default function EmployeeDirectory() {
-  return (
-    <>
-      <div className="directory">
-        <h1>Employee Directory Component</h1>
-      </div>
-    </>
-  );
-}
+const employeeDirectory = [
+  {
+    name: "Garth Marenghi",
+    title: "President and CEO",
+    number: "678-859-7469",
+    email: "g.marenghi@darkplace.org",
+    img: "../images/garth_marenghis.jpg",
+  },
+  {
+    name: "Dr. Rick Dagless M.D.",
+    title: "Cardio Pulmonary Specialist",
+    number: "678-923-4765",
+    email: "dr.rdagless@darkplace.org",
+    img: "../images/garth_marenghis_darkplace_rick.jpg",
+  },
+  {
+    name: "Dr. Elizabeth Asher",
+    title: "Chief Neurosurgeon",
+    number: "678-996-3025",
+    email: "dr.lizasher@darkplace.org",
+    img: "../images/garth_marenghis_darkplace_liz.jpg",
+  },
+  {
+    name: "Thorton Reed",
+    title: "CFO",
+    number: "678-931-8876",
+    email: "t.reed@darkplace.org",
+    img: "../images/garth_marenghis_darkplace_reed.jpg",
+  },
+  {
+    name: "Dr. Lucien Sanchez",
+    title: "Orthopedic Surgeon",
+    number: "678-891-3004",
+    email: "dr.lsanchez@darkplace.org",
+    img: "../images/garth_marenghis_darkplace_todd.jpg",
+  },
+  {
+    name: "Roy Trenneman",
+    title: "Software Engineer III",
+    number: "678-932-7015",
+    email: "r.trenneman@darkplace.org",
+    img: "../images/the_it_crowd_roy.jpg",
+  },
+  {
+    name: "Maurice Moss",
+    title: "Software Engineer II",
+    number: "0118-999-88199-9119-725-3",
+    email: "m.moss@darkplace.org",
+    img: "../images/the_it_crowd_moss.jpg",
+  },
+];
+
+module.export = employeeDirectory;
